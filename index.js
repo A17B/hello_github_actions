916962983604c87a27b16605b416cc8f88caf0ed
A17B <61550989+A17B@users.noreply.github.com>
2025-09-01 17:50:54 +0000

    initial versions

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
+try {
 const nameToGreet = core.getInput('name');
 console.log(`Hello ${nameToGreet} from Owner`);
 
 const ouput = `Hello ${nameToGreet} From Owner`;
 
 core.setOutput('greeting',ouput)
+
+    
+} catch (error) {
+    core.setFailed(error.message)
+}
